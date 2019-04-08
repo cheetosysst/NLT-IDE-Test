599958341894b3b27e723a7764f05ced15df6656
Thect <32976729+cheetosysst@users.noreply.github.com>
2019-04-08 13:05:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,68 +1,85 @@
 
-const {app, BrowserWindow, Menu } = require('electron')
-const electron = require('electron')
-const nav = require('./js/sidebar')
-const path = require('path')
+const {app, BrowserWindow, Menu, globalShortcut} = require('electron');
+const electron = require('electron');
+const nav = require('./js/sidebar');
+const menuCtrl = require('./js/menu.js');
+const path = require('path');
 const nativeImage = electron.nativeImage;
+const ipc = require('electron').ipcMain;
 
 let icon = nativeImage.createFromPath(path.join(__dirname, './src/icon.png'))
 
 let mainWindow = null
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ height: 600, width: 600, icon: icon })
-  mainWindow.loadFile('index.html')
+	mainWindow = new BrowserWindow({ height: 600, width: 600, icon: icon })
+	mainWindow.loadFile('index.html')
 
-  const template = [
-    {
-      label: 'File',
-      submenu: [
-        {
-          label: 'save',
-          click () {
-            console.log(mainWindow.webContents.executeJavaScript('console.log(document.textarea.mainTextArea.value)')[0])
-          }
-        }
-      ]
-    },
-    {
-      label: 'Edit',
-      submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
-        { role: 'copy' },
-        { role: 'paste' }
-      ]
-    },
-    {
-      label: 'Test',
-      submenu: [
-        {
-          label: 'I have a custom handler',
-          accelerate: 'CmdORCtrl+r',
-          click () {
-            console.log("123")
-          }
-        }
-      ]
-    }
-  ]
+	// globalShortcut.register('CommandOrControl+M', () =>{
+	// 	console.log('shortcut called');
+	// })
 
-  const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
+	const template = [
+		{
+			label: 'File',
+			submenu: [
+				{
+					label: 'save',
+					click () {
+						console.log(mainWindow.webContents.executeJavaScript('console.log(document.textarea.mainTextArea.value)'))
+					}
+				}
+			]
+		},
+		{
+			label: 'Edit',
+			submenu: [
+				{ role: 'undo' },
+				{ role: 'redo' },
+				{ type: 'separator' },
+				{ role: 'cut' },
+				{ role: 'copy' },
+				{ role: 'paste' }
+			]
+		},
+		{
+			label: 'menu',
+			submenu: [
+				{
+					label: 'Menu',
+					click () {
+						menuCtrl.close();
+					}
+				}
+			]
+		},
+		{
+			label: 'Test',
+			submenu: [
+				{
+					label: 'I have a custom handler',
+					accelerate: 'CmdORCtrl+r',
+					click () {
+						console.log("123")
+					}
+				}
+			]
+		}
+	]
+
+	const menu = Menu.buildFromTemplate(template)
+	Menu.setApplicationMenu(menu)
 })
 
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
+	if (mainWindow === null) {
+		createWindow()
+	}
 })
